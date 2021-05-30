9ef6e487a9454819cfdad06f878d5bb988b24135
Hugo Cedano <hugoce17@gmail.com>
2021-05-30 03:01:15 -0500

    syncing localstorage with vuex state

--- store/index.ts
+++ store/index.ts
@@ -7,6 +7,12 @@ const stringifyState = (state: any) => {
   return JSON.stringify({ ...state })
 }
 
+const syncStateToStorage = (state: any) => {
+  if (state.selectedAccount !== null) {
+    localStorage.setItem(state.selectedAccount, stringifyState(state))
+  }
+}
+
 export const state = () => ({
   isConnectDisabled: false,
   selectedAccount: null,
@@ -36,34 +42,38 @@ export const mutations: MutationTree<RootState> = {
   },
   resetState(state) {
     Object.assign(state, getDefaultState())
+    syncStateToStorage(state)
   },
   setChainId(state, id) {
     state.chainId = id
+    syncStateToStorage(state)
   },
   disableConnectButton(state, bStatus) {
     state.isConnectDisabled = bStatus
+    syncStateToStorage(state)
   },
   setSelectedAccount(state, selectedAccount) {
     state.selectedAccount = selectedAccount
+    syncStateToStorage(state)
   },
   setSelectedAccountEnsName(state, ensName) {
     state.selectedAccountEnsName = ensName
+    syncStateToStorage(state)
   },
   setNFTCollection(state, collection) {
     state.nftCollection = collection
+    syncStateToStorage(state)
   },
   pushToNFTCollection(
     state: { selectedAccount: string | null; nftCollection: any[] },
     nft
   ) {
     state.nftCollection = state.nftCollection.concat([nft])
-    console.log('STATE_COLLECTION: ', state.nftCollection)
-    if (state.selectedAccount !== null) {
-      localStorage.setItem(state.selectedAccount, stringifyState(state))
-    }
+    syncStateToStorage(state)
   },
   removeFromNFTCollectionByIndex(state, index) {
     state.nftCollection.splice(index, 1)
+    syncStateToStorage(state)
   },
 }
 
